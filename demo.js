4d63fb8c34d3f730182da83e7db7fdc7ba16d715
asadsayyad1 <asadsayyad85@gmail.com>
2021-09-13 23:04:38 +0530

    done

---
+++ demo.js
@@ -0,0 +1,20 @@
+//const age4 =20;
+const minAge =18;
+if( age4 >= minAge){
+    console.log("i am 20 year lod ");
+}
+else
+{
+    console.log(" i am child");
+
+}
+function Drive(num ){
+ if(num==18){
+     console.log("i am hero");
+ }
+ else{console
+
+ }
+
+}
+Drive(50)
